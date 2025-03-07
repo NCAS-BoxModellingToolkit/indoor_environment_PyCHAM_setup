ca3132369ab8e532d077452e7f5682d08e5418de
Simon O'Meara <simon.omeara@manchester.ac.uk>
2025-03-07 11:07:50 +0000

    Create __init__.py
    necessary for identifying as a package

---
+++ src/indoor_environment_PyCHAM_setup/__init__.py
@@ -0,0 +1,4 @@
+"""Creating the necessary input files to simulate an 
+indoor environment simulation for the CHemistry with 
+Aerosol Microphysics in Python (PyCHAM) model"""
+# this file marks the code as a package
